3d5570dc1aa16094758df1d7ae67f6be500c4105
kouheifujii <kohei.fujii@i-nations.tokyo>
2022-02-15 21:45:20 +0900

    monthly by prefeccure

--- practical/monthly_by_prefecture.sql
+++ practical/monthly_by_prefecture.sql
@@ -1,7 +1,8 @@
 /*
-都道府県別の平均客単価
+都道府県別の月別平均客単価
 都道府県ID
 都道府県名
+年月
 単価は少数第一位で四捨五入
 都道府県ID順に昇順
 */
@@ -9,6 +10,7 @@
 select
   p.id as prefecture_id,
   p.name as name,
+  date_format(o.order_time, "%Y%m") order_year_month,
   round(avg(o.amount), 0) avg
 from
   orders o
@@ -18,7 +20,10 @@ inner join
 inner join
   prefectures p
   on p.id = u.prefecture_id
-group by
-  p.id
-order by p.id
+group by -- 複数の重複条件設定
+  prefecture_id,
+  order_year_month
+order by
+  prefecture_id,
+  order_year_month
 ;
